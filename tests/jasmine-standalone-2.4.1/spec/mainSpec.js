6c2afb6650be6ad63389a1e9da77f3291c12ec58
Barry Gitarts <bgitarts@gmail.com>
2016-02-17 15:13:11 -0500

    mock data for valueStore.customerLifetimeValue

--- tests/jasmine-standalone-2.4.1/spec/mainSpec.js
+++ tests/jasmine-standalone-2.4.1/spec/mainSpec.js
@@ -9,3 +9,19 @@ describe("resetValues", function() {
     expect(valueStore.years).toEqual([]);
   });
 });
+
+describe("valueStore.customerLifetimeValue", function() {
+
+  beforeEach(function() {
+    valueStore.revenue = 30;
+    valueStore.margin = .1;
+    valueStore.churn = .1;
+    valueStore.discount = .1;
+    valueStore.costPerAcquisition = 843.3333333333334;
+    valueStore.lead = 3;
+    valueStore.hours = 5;
+    valueStore.hourlyRate = 50;
+    valueStore.numberOfLeads = 100;
+    valueStore.closing = 0.3;
+  })
+})
